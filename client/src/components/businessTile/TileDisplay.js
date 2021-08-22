ba97a3f40216f8c0e1029a05946a08aa913d6ecf
Jajuan Burton <jajuan.burton@live.com>
2021-08-22 10:39:52 -0400

    fix: see if lazy loading image component fixes problems in mobile

--- client/src/components/businessTile/TileDisplay.js
+++ client/src/components/businessTile/TileDisplay.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './tileDisplay.scss';
 import { Link } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 	const TileDisplay = (props) => (
 			<Link key={props.id} to={`/business/${props.id}`} className="business-tile">
 				<div className="business-photo-container">
-					<img className="tileImage" src={props.business.coverPhoto} alt={props.business.businessName}></img>		
+					<LazyLoadImage className="tileImage" src={props.business?.coverPhoto} alt={props.business.businessName}/>		
 				</div>
 
 				<div className="business-info">
 					<div className="business-name">
-						{props.business.businessName}
+						{props.business?.businessName}
 					</div>
 					<div className="business-category">
-						{props.business.businessCategory}
+						{props.business?.businessCategory}
 					</div>
 				</div>
 			</Link>
